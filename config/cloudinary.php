7189d14359f21cd13877de1625a4e041c8b3d373
Ahmed Benseidi <ahmed2100benseidi@gmail.com>
2026-03-16 13:27:49 +0100

    fix cloudinary config

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,8 +1,15 @@
 <?php
 
 return [
-    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-    'api_key'    => env('CLOUDINARY_API_KEY'),
-    'api_secret' => env('CLOUDINARY_API_SECRET'),
-    'cloud_url'  => env('CLOUDINARY_URL'),
+
+    'cloud_url' => env('CLOUDINARY_URL'),
+
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
+
 ];
