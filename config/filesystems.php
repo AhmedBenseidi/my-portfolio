33a4b74a32fb7de6f9d075fd6686ec8329c48fbf
Ahmed Benseidi <ahmed2100benseidi@gmail.com>
2026-03-17 08:56:24 +0100

    Cloudinary test14

--- config/filesystems.php
+++ config/filesystems.php
@@ -21,8 +21,17 @@ return [
         ],
 
         'cloudinary' => [
-            'driver' => 'cloudinary',
-        ],
+              'driver' => 'cloudinary',
+              'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+              'api_key'    => env('CLOUDINARY_API_KEY'),
+              'api_secret' => env('CLOUDINARY_API_SECRET'),
+    // الحزمة في بعض الإصدارات تبحث عن كلمة cloud كمصفوفة داخل القرص
+            'cloud' => [
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'api_key'    => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+                        ],
+],
 
     ],
 
